9d097d34206aa838b35b8cbd6981a7456fc1e260
shah-farheen <farheenshahdelhi@gmail.com>
2018-09-25 16:44:33 +0530

    feat(app) : test activity added

---
+++ app/src/main/java/co/classplus/cms/ui/test/MainActivity.java
@@ -0,0 +1,15 @@
+package co.classplus.cms.ui.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.classplus.cms.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
